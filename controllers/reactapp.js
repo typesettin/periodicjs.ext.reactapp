be40edebe13c840484eb261d2f3713aab7759941
yaw joseph etse <yaw.etse@gmail.com>
2017-07-14 07:02:28 -0400

    working on fixing manifests

--- controllers/reactapp.js
+++ controllers/reactapp.js
@@ -5,8 +5,7 @@ const Promisie = require('promisie');
 const utilities = require('../utilities');
 const UAParser = require('ua-parser-js');
 // console.log({ getParameterized });
-
-let components = utilities.controllerhelper.components;
+// let components = utilities.controllerhelper.components;
 const appSettings = periodic.settings;
 // console.log('periodic', periodic);
 appSettings.theme = periodic.settings.container.name;
@@ -72,8 +71,8 @@ function admin_index(req, res, next) {
           if (utilities.controllerhelper.unauthenticatedManifestSettings &&
             utilities.controllerhelper.unauthenticatedManifestSettings.containers &&
             Object.keys(utilities.controllerhelper.unauthenticatedManifestSettings.containers).length) {
-            let layoutPath = utilities.settings.findMatchingRoute( utilities.controllerhelper.unauthenticatedManifestSettings.containers, req._parsedOriginalUrl.pathname);
-            let manifest = (layoutPath) ?  utilities.controllerhelper.unauthenticatedManifestSettings.containers[layoutPath] : false;
+            let layoutPath = utilities.settings.findMatchingRoute(utilities.controllerhelper.unauthenticatedManifestSettings.containers, req._parsedOriginalUrl.pathname);
+            let manifest = (layoutPath) ? utilities.controllerhelper.unauthenticatedManifestSettings.containers[layoutPath] : false;
             // console.log({ layoutPath, manifest });
             return utilities.ssr_manifest({ layoutPath, manifest, req_url: req._parsedOriginalUrl.pathname, basename: extsettings.basename, });
           } else {
@@ -143,7 +142,7 @@ function loadUnauthenticatedManifest(req, res, next) {
   return utilities.controllerhelper.pullConfigurationSettings((req.query && req.query.refresh) ? 'unauthenticated' : false)
     .then(() => {
       let unauthenticated_manifest = Object.assign({}, utilities.controllerhelper.unauthenticatedManifestSettings);
-      console.log({ unauthenticated_manifest });
+      // console.log({ unauthenticated_manifest });
       if (req.query && req.query.refresh_log && req.query.refresh_log !== 'false') {
         logger.silly('reloaded unauthenticated manifest', { unauthenticated_manifest, });
       }
@@ -230,8 +229,8 @@ function loadNavigation(req, res, next) {
       }
       navigation.wrapper = extsettings.navigationLayout.wrapper;
       navigation.container = extsettings.navigationLayout.container;
-      console.log({ navigation }, 'req.session.userprivilegesdata', req.session.userprivilegesdata);
-      navigation.layout = utilities.controllerhelper.recursivePrivilegesFilter(Object.keys(req.session.userprivilegesdata ||{}), [navigation.layout, ])[0];
+      // console.log({ navigation }, 'req.session.userprivilegesdata', req.session.userprivilegesdata);
+      navigation.layout = utilities.controllerhelper.recursivePrivilegesFilter(Object.keys(req.session.userprivilegesdata || {}), [navigation.layout, ])[0];
       if (res && typeof res.send === 'function') {
         res.status(200).send({
           result: 'success',
@@ -247,7 +246,7 @@ function loadNavigation(req, res, next) {
 }
 
 function loadConfigurations(req, res) {
-  console.log('loading configs');
+  // console.log('loading configs');
   return utilities.controllerhelper.pullConfigurationSettings((req.query && req.query.refresh) ? true : false)
     .then(() => {
       if (req.query) delete req.query.refresh;
--- views/reactadmin/manifests/dashboard.manifest.js
+++ views/reactadmin/manifests/dashboard.manifest.js
@@ -1,39 +1,38 @@
 'use strict';
-const dbstatsLayout = require('../../../utility/detail_views/layouts/dbstats.manifest.layout.js');
+const utilities = require('../../../utilities');
+const reactapp = utilities.reactapp();
 
-module.exports = (periodic) => {
-  let reactadmin = periodic.app.controller.extension.reactadmin;
-  return {
-    containers: {
-      [`${reactadmin.manifest_prefix}dashboard`]: {
-        layout: {
-          component: 'div',
-          props: {
-            style: {
-              marginTop:'4.5rem',
-            },
+// const dbstatsLayout = require('../../../utilities/detail_views/layouts/dbstats.manifest.layout.js');
+
+module.exports = {
+  containers: {
+    [`${reactapp.manifest_prefix}dashboard`]: {
+      layout: {
+        component: 'div',
+        props: {
+          style: {
+            marginTop: '4.5rem',
           },
-          children: [
-            // breadcrumbs(['CRM', 'Customers']),
-            // developertabs('data'),
-            {
-              component: 'Container',
-              // children:'ok',
-              children: 
-                dbstatsLayout({}),
-              
-            },
-          ],
         },
-        'resources': {
-          contentstats: `${reactadmin.manifest_prefix}contentdata/standard/dbstats?format=json`,
-        },
-        'onFinish': 'render',
-        'pageData': {
-          'title': 'DBs Dashboard',
-          'navLabel': 'Dashboard',
-        },
+        children: [
+          // breadcrumbs(['CRM', 'Customers']),
+          // developertabs('data'),
+          {
+            component: 'Container',
+            children: 'ok',
+            // children: dbstatsLayout({}),
+
+          },
+        ],
+      },
+      'resources': {
+        contentstats: `${reactapp.manifest_prefix}contentdata/standard/dbstats?format=json`,
+      },
+      'onFinish': 'render',
+      'pageData': {
+        'title': 'DBs Dashboard',
+        'navLabel': 'Dashboard',
       },
     },
-  };
+  },
 };
