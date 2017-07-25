b47da23dbb8e397c89ac0851e770ec4b5a7f52e5
yaw joseph etse <yaw.etse@gmail.com>
2017-07-25 14:09:34 -0400

    support event listener for manifests

--- index.js
+++ index.js
@@ -8,18 +8,19 @@ const extsettings = utilities.reactapp().settings;
 module.exports = () => {
   return new Promise((resolve, reject) => {
     try {
-      if (extsettings && extsettings.includeCoreData && extsettings.includeCoreData.manifest) {
-        let task = setImmediate(() => {
-          utilities.controllerhelper.setCoreDataConfigurations();
-          clearImmediate(task);
-        });
-      }
-
-      Promisie.all(utilities.controllerhelper.pullConfigurationSettings(), utilities.controllerhelper.pullComponentSettings())
-      .then(() => {
-        logger.silly('MANIFEST SETTINGS LOADED');
-      },
-        logger.silly.bind(logger, 'settings error'));
+      periodic.status.on('configuration-complete', (status) => {
+        if (extsettings && extsettings.includeCoreData && extsettings.includeCoreData.manifest) {
+          let task = setImmediate(() => {
+            utilities.controllerhelper.setCoreDataConfigurations();
+            clearImmediate(task);
+          });
+        }
+        Promisie.all(utilities.controllerhelper.pullConfigurationSettings(), utilities.controllerhelper.pullComponentSettings())
+          .then(() => {
+              logger.silly('MANIFEST SETTINGS LOADED');
+            },
+            logger.silly.bind(logger, 'settings error'));
+      });
       return resolve(true);
     } catch (e) {
       return reject(e);
