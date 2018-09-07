8fd9000f1d4ca0a61d03b18ea4c7c6fa56236328
yaw joseph etse <yaw.etse@gmail.com>
2018-09-06 22:20:38 -0400

    adding responsive card to card server manifest

--- adminclient/src/server_manifest/card.js
+++ adminclient/src/server_manifest/card.js
@@ -45,7 +45,7 @@ function getContent(options) {
     children: (Array.isArray(options.content))
       ? options.content
       : [options.content, ],
-  } : null;
+  } :  null;
 }
 
 function getFooter(options) {
@@ -61,19 +61,36 @@ function getFooter(options) {
 }
 
 function getCard(options = { props: {}, }) {
-  const { componentProps, } = options;
+  const { componentProps, responsive, } = options;
   const { cardProps = {}, } = options.props;
+  const Image = getImage(options);
+  const Content = getContent(options);
+  const Footer = getFooter(options);
   // console.log('getCard',{ options,cardProps });
-  return Object.assign({
-    component: 'Card',
-    props: cardProps,
-    children: [
-      getTitle(options),
-      getImage(options),
-      getContent(options),
-      getFooter(options),
-    ],
-  }, componentProps);
+  return (responsive)
+    ? Object.assign({
+      component: 'ResponsiveCard',
+      props: Object.assign({
+        cardTitle: options.title,
+        iconRight: options.iconRight,
+        iconLeft: options.iconLeft,
+      }, cardProps),
+      children: []
+        .concat(Image ? Image.children : [])
+        .concat(Content ? Content.children : [])
+        .concat(Footer ? Footer.children : [])
+    },
+    componentProps)
+    : Object.assign({
+      component: 'Card',
+      props: cardProps,
+      children: [
+        getTitle(options),
+        Image,
+        Content,
+        Footer,
+      ],
+    }, componentProps);
 }
 
 module.exports = {
